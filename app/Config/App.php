117abc82f5d48d696fa5a6d3cc522a4219772af1
Saebyeok Chu <cuu2252@gmail.com>
2024-11-14 13:54:22 +0000

    save before merge

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     // public string $baseURL = 'http://localhost:8080/';
-    public string $baseURL = 'http://ojak.co.kr/';
+    public string $baseURL = 'https://ojak.co.kr/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
